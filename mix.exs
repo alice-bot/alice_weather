1c1070fd6d5c25ed7dad875a3e32e3fbc87d4a77
Mohammed M Khalid <5024526+mushfick@users.noreply.github.com>
2018-03-28 11:27:49 -0700

    Bump version after api_key fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AliceWeather.Mixfile do
   def project do
     [
       app: :alice_weather,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
